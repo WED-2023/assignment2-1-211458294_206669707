6a0dbe4bc9db1810bb7bafdc36ea60c27e6a5f4c
tiltang <127848200+tiltang@users.noreply.github.com>
2024-06-15 18:22:57 +0300

    my recipes- without logic on which recipes to display!

---
+++ src/pages/MyRecipesPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1>My Recipes</h1>
+      <RecipePreviewList :recipes="myrecipes" />
+    </div>
+  </template>
+  
+  <script>
+  import RecipePreviewList from '../components/RecipePreviewList.vue';
+  
+  export default {
+    name: 'MyRecipes',
+    components: {
+      RecipePreviewList
+    },
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
