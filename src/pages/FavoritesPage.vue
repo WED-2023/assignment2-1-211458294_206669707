d54003961a63f856f3a857553faad3f290f0412d
tiltang <127848200+tiltang@users.noreply.github.com>
2024-06-15 18:23:15 +0300

    favorite recipes- without logic on which recipes to display!

---
+++ src/pages/FavoritesPage.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+      <h1>Favorite Recipes</h1>
+      <RecipePreviewList :recipes="favoriteRecipes" />
+    </div>
+  </template>
+  
+  <script>
+  import RecipePreviewList from '../components/RecipePreviewList.vue';
+  
+  export default {
+    name: 'FavoritesPage',
+    components: {
+      RecipePreviewList
+    },
+  };
+  </script>
+  
+  <style scoped>
+  </style>
+  
